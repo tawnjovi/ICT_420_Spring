988f75d120fecbbabb3c9bfb7fc11f4c40f2eb28
tawnjovi <tpda224@uky.edu>
2020-05-01 22:55:40 -0400

    Update Contact_Sent.php

--- Contact_Sent.php
+++ Contact_Sent.php
@@ -1,10 +1,11 @@
 <!doctype html>
 <html>
 <head>
-<title>We Have Recieved Your Message</title>
+<title><?php
+print "We Have Received Your Message."; ?></title>
 </head>
 <body>
 <h1><?php
-print "Thank you, we have recieved your message and will respond aftera short period."; ?></h1>
+print "Thank you, we have received your message and will respond aftera short period."; ?></h1>
 </body>
 </html>
