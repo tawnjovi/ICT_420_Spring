dfda1a5be36be27414228e4743fa9981a4d02e70
tawnjovi <tpda224@uky.edu>
2020-05-01 22:39:18 -0400

    test

---
+++ Contact_Sent.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<title>We Have Recieved Your Message</title>
+</head>
+<body>
+<h1>Thank you, we have recieved your message and will respond after a short period.</h1>
+</body>
+</html>
